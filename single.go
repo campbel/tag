740b3c0f2625b1b2c121277fb9d1a83fcbccf845
Chris Campbell <chrs.cmpbl@gmail.com>
2015-11-12 15:08:48 -0600

    add logs for a "single" style tag

---
+++ single.go
@@ -0,0 +1,62 @@
+package tag
+
+import (
+	"encoding/json"
+	"log"
+)
+
+// Single represents a collection of values
+// denotes single as it contains only a single value for each entry
+// compared to doubles key-value and triples namespace-key-value
+type Single []string
+
+// NewSingleFromJSON creates a new Single from the JSON bytes
+func NewSingleFromJSON(bytes []byte) Single {
+	var single Single
+
+	err := json.Unmarshal(bytes, &single)
+	if err != nil {
+		log.Println("error creating single from JSON")
+		return Single{}
+	}
+
+	return single
+}
+
+// Add a value to the single, returning the new copy
+func (single Single) Add(val string) Single {
+	return append(single, val)
+}
+
+// Match determines if the given value matches the single
+func (single Single) Match(val string) bool {
+	for _, str := range single {
+		if str == val {
+			return true
+		}
+	}
+	return false
+}
+
+func (single Single) arrayAffinity(vals []string) int {
+	count := 0
+
+	for _, val := range vals {
+		if single.Match(val) {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return count
+}
+
+func (single Single) affinity(vals ...string) int {
+	return single.arrayAffinity(vals)
+}
+
+// Affinity determines the affinity score of the two singles
+func (single Single) Affinity(val Single) int {
+	return single.arrayAffinity(val)
+}
---
+++ single_test.go
@@ -0,0 +1,85 @@
+package tag
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestSingle(t *testing.T) {
+	single := Single{"foo", "bar", "baz"}.Add("qux")
+	expectedSingle := Single{"foo", "bar", "baz", "qux"}
+
+	if len(single) != 4 {
+		t.Errorf("expected single length to be %d but was %d", 4, len(single))
+	}
+
+	if !reflect.DeepEqual(single, expectedSingle) {
+		t.Errorf("expected single to be equal to %v but was %v", expectedSingle, single)
+	}
+
+}
+
+func TestMatch(t *testing.T) {
+	single := Single{"foo", "bar", "baz"}
+
+	for _, val := range single {
+		if !single.Match(val) {
+			t.Errorf("expected single to match %s but didn't", val)
+		}
+	}
+
+	if single.Match("qux") {
+		t.Error("expected single to NOT match qux but did")
+	}
+}
+
+func TestAffinity(t *testing.T) {
+	single := Single{"foo", "bar", "baz"}
+
+	affinity := single.affinity("foo", "bar", "baz")
+	if affinity != 3 {
+		t.Errorf("expected affinity to be %d but was %d", 3, affinity)
+	}
+
+	affinity = single.affinity("foo", "bar")
+	if affinity != 2 {
+		t.Errorf("expected affinity to be %d but was %d", 2, affinity)
+	}
+
+	affinity = single.affinity("foo", "bar", "qux")
+	if affinity != 1 {
+		t.Errorf("expected affinity to be %d but was %d", 1, affinity)
+	}
+}
+
+func TestNewFromJSON(t *testing.T) {
+
+	bytes := []byte(`["foo", "bar", "baz"]`)
+
+	single := NewSingleFromJSON(bytes)
+	expectedSingle := Single{"foo", "bar", "baz"}
+
+	if !reflect.DeepEqual(single, expectedSingle) {
+		t.Errorf("expected single to be equal to %v but was %v", expectedSingle, single)
+	}
+
+}
+
+func TestJSONSingleIsLikeArray(t *testing.T) {
+	bytes := []byte(`{"tags": ["foo", "bar", "baz"]}`)
+
+	var obj map[string]Single
+
+	err := json.Unmarshal(bytes, &obj)
+	if err != nil {
+		t.Error("failed to parse tags from json")
+	}
+
+	single := obj["tags"]
+	expectedSingle := Single{"foo", "bar", "baz"}
+
+	if !reflect.DeepEqual(single, expectedSingle) {
+		t.Errorf("expected single to be equal to %v but was %v", expectedSingle, single)
+	}
+}
